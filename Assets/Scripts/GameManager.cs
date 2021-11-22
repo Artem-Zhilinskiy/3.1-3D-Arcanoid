ed1730149f606f6cb7b8edc2d063f6bf6fb2b311
Artem-Zhilinskiy <90002515+Artem-Zhilinskiy@users.noreply.github.com>
2021-11-22 20:02:51 +0300

    22.11.21
    Сделал движение платформы. Вынес параметр скорости движения платформ в GameManager, чтобы можно было редактировать из инспектора.
    Следующее - сделать ограничение движения платформы, чтобы она не выходила за пределы трубы.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arcanoid
+{
+    public class GameManager : MonoBehaviour
+    {
+        [Tooltip("Скорость движения платформ"), SerializeField]
+        public int _platformSpeed = 5;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
--- Assets/Scripts/Platform1InputControls.cs
+++ Assets/Scripts/Platform1InputControls.cs
@@ -3,59 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.InputSystem.InputAction;
 
-public class Platform1InputControls : MonoBehaviour
+namespace Arcanoid
 {
-    //1. Первое что нужно сделать при создании скрипта управления в NewInputSystem
-    public PlatformControls1 controls;
-
-    //2. Второе, что нужно сделать - инициализировать controls в Awake
-    private void Awake()
+    public class Platform1InputControls : MonoBehaviour
     {
-        controls = new PlatformControls1();
-    }
+        //Импорт настроек из GameManager
+        //Объявление переменной, чтобы можно было получить доступ в других методах. Надо в инспекторе Unity добавить.
+        [SerializeField] private GameManager GameManager;
 
-    //3. Активировать в OnEnable ActionMap
-    private void OnEnable()
-    {
-        controls.ActionMap.Enable();
+        //1. Первое что нужно сделать при создании скрипта управления в NewInputSystem
+        public PlatformControls1 controls;
 
-        //Подключить события
-        //controls.ActionMap.Movement.performed += OnMovement;
-        controls.ActionMap.Launch.performed += OnLaunch;
-    }
+        //2. Второе, что нужно сделать - инициализировать controls в Awake
+        private void Awake()
+        {
+            controls = new PlatformControls1();
+        }
 
-    //4. Дезактивировать в OnDisable
-    private void OnDisable()
-    {
-        controls.ActionMap.Launch.performed -= OnLaunch;
-        controls.ActionMap.Disable();
-    }
+        //3. Активировать в OnEnable ActionMap
+        private void OnEnable()
+        {
+            controls.ActionMap.Enable();
 
-    //5. Написать обработчики событий
-    /* В OnMovement ничего не писать, потому что движение обрабатывается в Update
-    public void OnMovement (CallbackContext context)
-    {
-        //Дальше перейти в Update.
+            //Подключить события
+            //controls.ActionMap.Movement.performed += OnMovement;
+            controls.ActionMap.Launch.performed += OnLaunch;
+        }
 
-    }
-    */
+        //4. Дезактивировать в OnDisable
+        private void OnDisable()
+        {
+            controls.ActionMap.Launch.performed -= OnLaunch;
+            controls.ActionMap.Disable();
+        }
 
-    public void OnLaunch (CallbackContext context)
-    {
+        //5. Написать обработчики событий
+        /* В OnMovement ничего не писать, потому что движение обрабатывается в Update
+        public void OnMovement (CallbackContext context)
+        {
+            //Дальше перейти в Update.
 
-    }
+        }
+        */
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        public void OnLaunch(CallbackContext context)
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        //6. Обработка движения
-        var value = controls.ActionMap.Movement.ReadValue<Vector2>();
-        transform.position += new Vector3(value.x, 0, value.y) * Time.deltaTime;
+        }
+
+        // Start is called before the first frame update
+        public void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //6. Обработка движения
+            var value = controls.ActionMap.Movement.ReadValue<Vector2>();
+            transform.position += new Vector3(0, value.y, value.x) * Time.deltaTime * GameManager._platformSpeed;
+            //transform.position += new Vector3(0, value.y, value.x) * Time.deltaTime * GameObject.Find("GameManager").GetComponent<GameManager>()._platformSpeed;
+        }
     }
 }
